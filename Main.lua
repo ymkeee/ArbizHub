0f973ecb9d4355d987c1b4a6d92083faf546730c
ymkeee <forfunpaychilled@gmail.com>
2025-12-28 17:20:45 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,11 @@
+local placeId = game.PlaceId
 
+if placeId == 85085406792679 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ymkeee/ArbizHub/refs/heads/main/Game1.lua"))()
+elseif placeId == 108447089422962 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ymkeee/sierra/refs/heads/main/game1.lua"))()
+elseif placeId == 129864072130807 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ymkeee/sierra/refs/heads/main/game2.lua"))()
+elseif placeId == 129854327403392 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ymkeee/sierra/refs/heads/main/game3.lua"))()
+end
