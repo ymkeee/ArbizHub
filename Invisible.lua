2a5e4b77540893bb82330bb2b7fe407f920f25b4
ymkeee <forfunpaychilled@gmail.com>
2026-01-13 13:50:16 +0300

    Update Invisible.lua

--- Invisible.lua
+++ Invisible.lua
@@ -1,1 +1,16 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/192b0235b9041cac185d337c3db955d6577c7c75314a870960fb0983ceabf631/download"))()
+JunkieCore = nil
+JD = nil
+JunkieProtected = nil
+local url = "https://api.jnkie.com/api/v1/luascripts/public/192b0235b9041cac185d337c3db955d6577c7c75314a870960fb0983ceabf631/download"
+
+local code = game:HttpGet(url, true)
+if code then
+    loadstring(code)()
+else
+    local code2 = game:HttpGetAsync(url, true)
+    if code2 then
+        loadstring(code2)()
+    else
+        print("Failed to load script, maybe executor problem.")
+    end
+end
